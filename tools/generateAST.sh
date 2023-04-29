ab42caeb3c3b76aa90216f025cbf39c6b9ea8b95
josemarialanda <josemaria.landa@gmail.com>
2023-04-29 20:45:00 +0100

    force move when autogenerating code

--- tools/generateAST.sh
+++ tools/generateAST.sh
@@ -1,3 +1,3 @@
 python ./generateAST.py "."
-mv ./expr.py ../src/parser/expr.py
-mv ./stmt.py ../src/parser/stmt.py
+mv --force ./expr.py ../src/parser/expr.py
+mv --force ./stmt.py ../src/parser/stmt.py
